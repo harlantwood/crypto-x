77baf35ef8071ef279d4f743175302c998986e11
Pelle Braendgaard <pelle@stakeventures.com>
2017-02-16 22:59:28 -0600

    requet push notifications

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,6 @@ import * as AppActions from '../actions/AppActions'
 import { Connect, SimpleSigner } from 'uport-connect'
 const uport = new Connect('CryptoX', {
   clientId: '0xe2fef711a5988fbe84b806d4817197f033dde050',
-  notifications: true,
   signer: SimpleSigner('4894506ba6ed1a2d21cb11331620784ad1ff9adf1676dc2720de5435dcf76ac2')
 })
 
@@ -24,7 +23,7 @@ class Navbar extends Component {
   signInbtnClick () {
     console.log(uport)
     uport
-      .requestCredentials({requested: ['name', 'phone', 'country']})
+      .requestCredentials({requested: ['name', 'phone', 'country'], notifications: true})
       .then((credentials) => {
         this.props.actions.connectUport(credentials)
         console.log(this.props, this.state)
